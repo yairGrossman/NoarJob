f664d6c87a7813bfa1e5b184e7faae441c56b52c
Michel Tkatch <misheltkach@gmail.com>
2022-11-23 08:37:46 +0200

    הוספת מסך של מערכת ATS

---
+++ NoarJobUI/ATSPage.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace NoarJobUI
+{
+    partial class ATSPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TapTypePanel = new System.Windows.Forms.Panel();
+            this.NotCatalogedBtn = new System.Windows.Forms.Button();
+            this.FoundSuitableBtn = new System.Windows.Forms.Button();
+            this.NotFoundSuitableBtn = new System.Windows.Forms.Button();
+            this.TelephoneInterviewBאמ = new System.Windows.Forms.Button();
+            this.ProfessionalInterviewBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TapTypePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TapTypePanel
+            // 
+            this.TapTypePanel.Controls.Add(this.button1);
+            this.TapTypePanel.Controls.Add(this.ProfessionalInterviewBtn);
+            this.TapTypePanel.Controls.Add(this.TelephoneInterviewBאמ);
+            this.TapTypePanel.Controls.Add(this.NotFoundSuitableBtn);
+            this.TapTypePanel.Controls.Add(this.FoundSuitableBtn);
+            this.TapTypePanel.Controls.Add(this.NotCatalogedBtn);
+            this.TapTypePanel.Location = new System.Drawing.Point(191, 60);
+            this.TapTypePanel.Name = "TapTypePanel";
+            this.TapTypePanel.Size = new System.Drawing.Size(1129, 86);
+            this.TapTypePanel.TabIndex = 0;
+            // 
+            // NotCatalogedBtn
+            // 
+            this.NotCatalogedBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(179)))), ((int)(((byte)(144)))));
+            this.NotCatalogedBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.NotCatalogedBtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.NotCatalogedBtn.Location = new System.Drawing.Point(940, 1);
+            this.NotCatalogedBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.NotCatalogedBtn.Name = "NotCatalogedBtn";
+            this.NotCatalogedBtn.Size = new System.Drawing.Size(188, 85);
+            this.NotCatalogedBtn.TabIndex = 6;
+            this.NotCatalogedBtn.Text = "לא קוטלגו\r\n(הגישו מועמדות)";
+            this.NotCatalogedBtn.UseVisualStyleBackColor = false;
+            // 
+            // FoundSuitableBtn
+            // 
+            this.FoundSuitableBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(179)))), ((int)(((byte)(144)))));
+            this.FoundSuitableBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FoundSuitableBtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.FoundSuitableBtn.Location = new System.Drawing.Point(752, 1);
+            this.FoundSuitableBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.FoundSuitableBtn.Name = "FoundSuitableBtn";
+            this.FoundSuitableBtn.Size = new System.Drawing.Size(188, 85);
+            this.FoundSuitableBtn.TabIndex = 7;
+            this.FoundSuitableBtn.Text = "נמצאו מתאימים למשרה";
+            this.FoundSuitableBtn.UseVisualStyleBackColor = false;
+            // 
+            // NotFoundSuitableBtn
+            // 
+            this.NotFoundSuitableBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(179)))), ((int)(((byte)(144)))));
+            this.NotFoundSuitableBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.NotFoundSuitableBtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.NotFoundSuitableBtn.Location = new System.Drawing.Point(564, 1);
+            this.NotFoundSuitableBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.NotFoundSuitableBtn.Name = "NotFoundSuitableBtn";
+            this.NotFoundSuitableBtn.Size = new System.Drawing.Size(188, 85);
+            this.NotFoundSuitableBtn.TabIndex = 8;
+            this.NotFoundSuitableBtn.Text = "נמצאו לא מתאימים למשרה";
+            this.NotFoundSuitableBtn.UseVisualStyleBackColor = false;
+            // 
+            // TelephoneInterviewBאמ
+            // 
+            this.TelephoneInterviewBאמ.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(179)))), ((int)(((byte)(144)))));
+            this.TelephoneInterviewBאמ.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.TelephoneInterviewBאמ.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TelephoneInterviewBאמ.Location = new System.Drawing.Point(376, 1);
+            this.TelephoneInterviewBאמ.Margin = new System.Windows.Forms.Padding(0);
+            this.TelephoneInterviewBאמ.Name = "TelephoneInterviewBאמ";
+            this.TelephoneInterviewBאמ.Size = new System.Drawing.Size(188, 85);
+            this.TelephoneInterviewBאמ.TabIndex = 9;
+            this.TelephoneInterviewBאמ.Text = "עברו ראיון טלפוני ראשוני";
+            this.TelephoneInterviewBאמ.UseVisualStyleBackColor = false;
+            // 
+            // ProfessionalInterviewBtn
+            // 
+            this.ProfessionalInterviewBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(179)))), ((int)(((byte)(144)))));
+            this.ProfessionalInterviewBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.ProfessionalInterviewBtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.ProfessionalInterviewBtn.Location = new System.Drawing.Point(188, 1);
+            this.ProfessionalInterviewBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.ProfessionalInterviewBtn.Name = "ProfessionalInterviewBtn";
+            this.ProfessionalInterviewBtn.Size = new System.Drawing.Size(188, 85);
+            this.ProfessionalInterviewBtn.TabIndex = 10;
+            this.ProfessionalInterviewBtn.Text = "עברו ראיון מקצועי";
+            this.ProfessionalInterviewBtn.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(179)))), ((int)(((byte)(144)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button1.Location = new System.Drawing.Point(0, 1);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 85);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "עברו חתימה על חוזה העסקה";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // ATSPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(248)))), ((int)(((byte)(232)))));
+            this.ClientSize = new System.Drawing.Size(1496, 476);
+            this.Controls.Add(this.TapTypePanel);
+            this.Name = "ATSPage";
+            this.Text = "ATSPage";
+            this.TapTypePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TapTypePanel;
+        public System.Windows.Forms.Button NotCatalogedBtn;
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button ProfessionalInterviewBtn;
+        public System.Windows.Forms.Button TelephoneInterviewBאמ;
+        public System.Windows.Forms.Button NotFoundSuitableBtn;
+        public System.Windows.Forms.Button FoundSuitableBtn;
+    }
+}
