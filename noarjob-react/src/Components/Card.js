802671b6fd4d5e3f1e0bfa01c0153b5f13a1b387
yairGrossman <yair.grsm@gmail.com>
2023-03-14 17:01:07 +0200

    שינוי עיצוב לכרטיס

--- noarjob-react/src/Components/Card.js
+++ noarjob-react/src/Components/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => {
       <div className="container pb-4 pt-3 h-70">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className={"col-12 col-md-8 col-lg-6 " + props.cardSize}>
-            <div className="card myCard" style={{ borderRadius: "1rem" }}>
+            <div className="card myCard rounded-4">
               <div className="card-body p-5 text-center">{props.children}</div>
             </div>
           </div>
